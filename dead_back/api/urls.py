1ffa273ff56d4b09698e52652e763f48874f13f9
Raspberrynani <daryl.goh@isj.technology.com>
2025-02-25 14:18:13 +0800

    removed old artifacts

--- dead_back/api/urls.py
+++
@@ -1,9 +0,0 @@
-# backend/api/urls.py
-from django.urls import path
-from .views import get_entries  # Ensure this view exists in api/views.py
-
-urlpatterns = [
-    path('entries/', get_entries, name='entries'),
-    path('api/', include('pong.urls')),
-]
-
