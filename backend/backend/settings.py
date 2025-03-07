5b1ea3e478edc61c715abbc89b89ec890acf70f2
Raspberrynani <daryl.goh@isj.technology.com>
2025-03-07 20:12:11 +0800

    FINAL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,10 +52,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_ALLOW_ALL = False
-CORS_ALLOWED_ORIGINS = [ 
-    "https://localhost:8001", 
-    "http://localhost:8001", 
-]
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://localhost:8444",
